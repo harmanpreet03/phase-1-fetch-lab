9882b20cf747067238702caaf78758578a05e9e0
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-08-04 11:06:50 -0400

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json()) // Call response.json() with parentheses
+    .then((books) => renderBooks(books)); // Pass the fetched JSON data to renderBooks()
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+  const main = document.querySelector("main");
+  books.forEach((books) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = books.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
